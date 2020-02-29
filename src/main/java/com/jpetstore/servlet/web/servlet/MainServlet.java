773e5c8dcf2936252875d5aa1109456dd7758984
liujianyu <1173770418@qq.com>
2020-02-29 15:38:43 +0800

    One try

--- src/main/java/com/jpetstore/servlet/web/servlet/MainServlet.java
+++ src/main/java/com/jpetstore/servlet/web/servlet/MainServlet.java
@@ -1,13 +1,13 @@
 package com.jpetstore.servlet.web.servlet;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "MainServlet")
+
 public class MainServlet extends HttpServlet {
     private static final String MAIN ="/WEB-INF/jsp/catalog/Main.jsp";
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
